cc8f469045520bd45c9e79db1d0ffe40915313c4
afish <owengoal2008@163.com>
2017-07-07 20:59:43 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -18,7 +18,7 @@ class Admin::JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to admin_job_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -49,7 +49,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 
 end
